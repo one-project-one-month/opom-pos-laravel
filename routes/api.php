17bc93be5e88bc9df7eb5d426e0b7f8ee6ec472a
ellie-swe <ellieswe.aswe@gmail.com>
2025-07-10 22:40:30 +0630

    merge-with-dev-71025

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,10 @@
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\BrandController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
