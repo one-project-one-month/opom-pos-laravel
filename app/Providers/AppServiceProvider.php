153e74fb307a474f9041507f760c3bffd1255171
Kyaw Mg Mg Lwin <kyawmgmgliwn146018@gmail.com>
2025-07-30 11:50:02 +0630

    update Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+         if (env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
         // Password::resetResponse(function ($request, $response) {
         //     return response()->json(['message' => trans($response)]);
         // });
