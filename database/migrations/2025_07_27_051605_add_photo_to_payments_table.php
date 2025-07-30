2d14fecf004c84300660b499f5f5da04bf2f0b5c
Kyaw Mg Mg Lwin <kyawmgmglwin146018@gmail.com>
2025-07-30 10:45:49 +0630

    Update 2025_07_27_051605_add_photo_to_payments_table.php

--- database/migrations/2025_07_27_051605_add_photo_to_payments_table.php
+++ database/migrations/2025_07_27_051605_add_photo_to_payments_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('payments', function (Blueprint $table) {
-            $table->string('photo')->nullable;
+            $table->string('photo')->nullable();
         });
     }
 
