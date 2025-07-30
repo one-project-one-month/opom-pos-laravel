7d40a317f820793fc7a8f3edb3bda7451fe0d8a0
Kyaw Mg Mg Lwin <kyawmgmgliwn146018@gmail.com>
2025-07-30 15:48:20 +0630

    update provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-         if (env('APP_ENV') === 'production') {
+        if (app()->environment('production')) {
         URL::forceScheme('https');
     }
         // Password::resetResponse(function ($request, $response) {
