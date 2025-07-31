99dd946293427750dbb1ad5ef2ee9485744914ad
Kyaw Mg Mg Lwin <kyawmgmgliwn146018@gmail.com>
2025-07-31 14:12:10 +0630

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,8 +2,71 @@
 
 namespace App\Providers\Filament;
 
+// use Filament\Pages;
+// use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+// use Filament\Panel;
+// use Filament\Widgets;
+// use Filament\PanelProvider;
+// use Filament\Support\Colors\Color;
+// use Filament\Http\Middleware\Authenticate;
+// use Illuminate\Session\Middleware\StartSession;
+// use Illuminate\Cookie\Middleware\EncryptCookies;
+// use Filament\Http\Middleware\AuthenticateSession;
+// use Illuminate\Routing\Middleware\SubstituteBindings;
+// use Illuminate\View\Middleware\ShareErrorsFromSession;
+// use Filament\Http\Middleware\DisableBladeIconComponents;
+// use Filament\Http\Middleware\DispatchServingFilamentEvent;
+// use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+// use App\Filament\Resources\AdminResource\Pages\SalesReport;
+// use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+
+
+// class AdminPanelProvider extends PanelProvider
+// {
+//     public function panel(Panel $panel): Panel
+//     {
+//         return $panel
+//             ->default()
+//             ->id('admin')
+//             ->path('admin')
+//             ->login()
+//             ->colors([
+//                 'primary' => Color::Amber,
+//             ])
+//             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+//             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+//             ->pages([
+//                 Pages\Dashboard::class,
+//                 SalesReport::class,
+//             ])
+//             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+//             ->widgets([
+//                 Widgets\AccountWidget::class,
+//                 Widgets\FilamentInfoWidget::class,
+//             ])
+//             ->middleware([
+//                 EncryptCookies::class,
+//                 AddQueuedCookiesToResponse::class,
+//                 StartSession::class,
+//                 AuthenticateSession::class,
+//                 ShareErrorsFromSession::class,
+//                 VerifyCsrfToken::class,
+//                 SubstituteBindings::class,
+//                 DisableBladeIconComponents::class,
+//                 DispatchServingFilamentEvent::class,
+//             ])
+//             ->plugins([
+//                 FilamentShieldPlugin::make(),
+//             ])
+//             ->authMiddleware([
+//                 Authenticate::class,
+//             ]);
+//     }
+// }
+
+namespace App\Providers\Filament;
+
 use Filament\Pages;
-use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
@@ -11,15 +74,12 @@ use Filament\Support\Colors\Color;
 use Filament\Http\Middleware\Authenticate;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Filament\Http\Middleware\AuthenticateSession;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-use App\Filament\Resources\AdminResource\Pages\SalesReport;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -37,7 +97,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
-                SalesReport::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
@@ -46,20 +105,18 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->middleware([
                 EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
                 StartSession::class,
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
-            ->plugins([
-                FilamentShieldPlugin::make(),
-            ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                FilamentShieldPlugin::make(),
             ]);
     }
 }
