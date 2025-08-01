b1093f60bbc87bcab819a92e0006d7e0de35c680
Kyaw Mg Mg Lwin <kyawmgmgliwn146018@gmail.com>
2025-08-01 22:01:57 +0630

    cors

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,29 @@
 <?php
+// return [
+//        // API route တွေမှာ CORS ကို enable လုပ်မယ်။
+//     'allowed_methods' => ['*'],    // GET, POST, PUT, DELETE အကုန်လုံးခွင့်ပြုမယ်။
+//     'allowed_origins' => ['*'],    // ဘယ် domain မှာမဆိုခွင့်ပြုမယ်။
+//     'allowed_headers' => ['*'],    // headers အကုန်လုံးခွင့်ပြုမယ်။
+//     'exposed_headers' => [],
+//     'max_age' => 0,
+//     'supports_credentials' => false,
+// ];
 return [
-//     'paths' => [
-//     'api/*',
-//     'filament/*',
-//     'admin/*',
-//     'login',
-//     'logout',
-// ],
 
-    // 'paths' => ['api/*',
-    //             'filament/*',
-    //             'admin/*',
-    //             'login',
-    //             'logout',],          // API route တွေမှာ CORS ကို enable လုပ်မယ်။
-    'allowed_methods' => ['*'],    // GET, POST, PUT, DELETE အကုန်လုံးခွင့်ပြုမယ်။
-    'allowed_origins' => ['*'],    // ဘယ် domain မှာမဆိုခွင့်ပြုမယ်။
-    'allowed_headers' => ['*'],    // headers အကုန်လုံးခွင့်ပြုမယ်။
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['http://localhost:3000'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
+
 ];
