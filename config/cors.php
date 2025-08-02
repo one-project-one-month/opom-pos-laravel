ca0b1a6d95508002148a30dadedbbbd59d49464f
Kyaw Mg Mg Lwin <kyawmgmgliwn146018@gmail.com>
2025-08-02 12:07:58 +0630

    update

--- config/cors.php
+++ config/cors.php
@@ -1,31 +1,22 @@
 <?php
-// return [
-//        // API route တွေမှာ CORS ကို enable လုပ်မယ်။
-//     'allowed_methods' => ['*'],    // GET, POST, PUT, DELETE အကုန်လုံးခွင့်ပြုမယ်။
-//     'allowed_origins' => ['*'],    // ဘယ် domain မှာမဆိုခွင့်ပြုမယ်။
-//     'allowed_headers' => ['*'],    // headers အကုန်လုံးခွင့်ပြုမယ်။
-//     'exposed_headers' => [],
-//     'max_age' => 0,
-//     'supports_credentials' => false,
-// ];
 return [
 
 
-    'paths' => ['api/*', 'login', 'sanctum/csrf-cookie', 'v1/*'], // make sure 'login' or your API path is here
+    'paths' => ['api/*', 'login', 'sanctum/csrf-cookie', 'v1/*'], 
 
-    'allowed_methods' => ['*'], // or ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE']
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'], // OR '*' only for dev
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'], // for Content-Type, Authorization, etc
+    'allowed_headers' => ['*'], 
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false, // important if using cookies / auth headers
+    'supports_credentials' => false,
 
 ];
 
